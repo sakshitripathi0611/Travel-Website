519499314c36f447b6da3fef105969911152bb4d
sakshitripathi0611 <113855374+sakshitripathi0611@users.noreply.github.com>
2024-08-25 21:43:03 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
 
+document.querySelector(".nav-list").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
 let i = 0;
